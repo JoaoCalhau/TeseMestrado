4cf14fc325c8de73aa485235c4741ededcddec09
João Calhau <m36764@alunos.uevora.pt>
2018-01-23 13:24:50 +0000

    Resuming implementation of doubly linked list hash tables

--- data.structures/src/DoublyLinkedHashTable.java
+++ data.structures/src/DoublyLinkedHashTable.java
@@ -12,4 +12,37 @@ public class DoublyLinkedHashTable {
         for(int i = 0; i < SIZE; i++)
             table[i] = new DoublyLinkedList();
     }
+
+    public DoublyLinkedHashTable(int SIZE) {
+        this.SIZE = SIZE;
+        OCCUPIED = 0;
+        table = new DoublyLinkedList[SIZE];
+
+        for(int i = 0; i < SIZE; i++)
+            table[i] = new DoublyLinkedList();
+    }
+
+    public DoublyLinkedList[] getTable() {
+        return table;
+    }
+
+    public void setTable(DoublyLinkedList[] table) {
+        this.table = table;
+    }
+
+    public int getSIZE() {
+        return SIZE;
+    }
+
+    public void setSIZE(int SIZE) {
+        this.SIZE = SIZE;
+    }
+
+    public int getOCCUPIED() {
+        return OCCUPIED;
+    }
+
+    public void setOCCUPIED(int OCCUPIED) {
+        this.OCCUPIED = OCCUPIED;
+    }
 }
